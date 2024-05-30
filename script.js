227d1c8fff1e1f96debc632d3ec913c2a9017c3d
Dev Harsh <devharsh466@gmail.com>
2024-05-31 00:29:45 +0530

    adding reset position functionality and beautifying it

--- script.js
+++ script.js
@@ -14,14 +14,35 @@ const createScene = function() {
 
     // Add lights to the scene
     const light1 = new BABYLON.HemisphericLight("light1", new BABYLON.Vector3(1, 1, 0), scene);
+    const light2 = new BABYLON.PointLight("light2", new BABYLON.Vector3(0, 50, 0), scene);
 
     // Create a room (floor)
-    const roomSize = 100;
-    const floor = BABYLON.MeshBuilder.CreateGround("floor", {width: roomSize, height: roomSize}, scene);
+    const roomSizeInFeet = 100; // Room size in square feet
+    const floor = BABYLON.MeshBuilder.CreateGround("floor", {width: roomSizeInFeet, height: roomSizeInFeet}, scene);
+
+    // Apply a texture to the floor
+    const floorMaterial = new BABYLON.StandardMaterial("floorMaterial", scene);
+    floorMaterial.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/floor.png", scene);
+    floor.material = floorMaterial;
 
     // Add a box to the scene
     const box = BABYLON.MeshBuilder.CreateBox("box", {size: 5}, scene);
-    box.position.y = 2.5;
+    box.position.y = 2.5; // Position the box above the ground
+
+    // Apply a texture material to the box
+    const boxMaterial = new BABYLON.StandardMaterial("boxMaterial", scene);
+    boxMaterial.diffuseTexture = new BABYLON.Texture("https://www.babylonjs-playground.com/textures/crate.png", scene);
+    box.material = boxMaterial;
+
+    // Add a skybox
+    const skybox = BABYLON.MeshBuilder.CreateBox("skyBox", {size: 500}, scene);
+    const skyboxMaterial = new BABYLON.StandardMaterial("skyBox", scene);
+    skyboxMaterial.backFaceCulling = false;
+    skyboxMaterial.reflectionTexture = new BABYLON.CubeTexture("https://playground.babylonjs.com/textures/skybox", scene);
+    skyboxMaterial.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
+    skyboxMaterial.diffuseColor = new BABYLON.Color3(0, 0, 0);
+    skyboxMaterial.specularColor = new BABYLON.Color3(0, 0, 0);
+    skybox.material = skyboxMaterial;
 
     // Add keyboard controls to move the box
     scene.onKeyboardObservable.add((kbInfo) => {
@@ -43,6 +64,11 @@ const createScene = function() {
                 }
                 break;
         }
+    });
+
+    // Reset button to move the box back to the center
+    document.getElementById("resetButton").addEventListener("click", () => {
+        box.position = new BABYLON.Vector3(0, 2.5, 0);
     });
 
     return scene;
